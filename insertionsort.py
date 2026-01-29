e01058b2b20c7c164078e2017a95aa7e7f211dc8
Khushi Gosavi <gosavikhushi62@gmail.com>
2026-01-29 10:55:05 +0530

    Add files via upload

--- insertionsort.py
+++ insertionsort.py
@@ -1,13 +1,32 @@
 import random
-l=[random.randint(1,100) for _ in range(10)]
-print(l)
-n=len(l)
-for i in range(1,n):
-    j=i-1
-    v=l[i]
-    while j>=0 and l[j]>v:
-        l[j+1]=l[j]
-        j=j-1
-    l[j+1]=v
+import time
+import matplotlib.pyplot as plt
 
-print(l)
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+n_list = [5000, 6000, 7000, 8000, 9000, 10000]
+sort_time = []
+
+for n in n_list:
+    l = [random.randint(1, 100) for _ in range(n)]
+    s_t = time.time()
+    insertion_sort(l)
+    e_t = time.time()
+    sort_time.append(e_t - s_t)
+plt.plot(n_list, sort_time, marker='x') 
+plt.xlabel("Number of elements(n)")
+plt.ylabel("Time taken in seconds)")
+plt.title("Insertion Sort sort: Time Complexity Analysis")
+plt.grid(True)
+plt.savefig("insertion_sort.time_complexity.png", dpi=300,bbox_inches='tight')
+plt.show()
+
+print("Input sizes:", n_list)
+print("Sorting times:", sort_time)
