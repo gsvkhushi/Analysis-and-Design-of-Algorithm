a48bde1ee44b1604e29b3ffb7bb306562809ff17
Khushi Gosavi <gosavikhushi62@gmail.com>
2026-02-20 11:50:56 +0530

    Add files via upload

---
+++ heap_sort.py
@@ -0,0 +1,27 @@
+def max_heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        max_heapify(arr, n, largest)
+
+
+def build_max_heap(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        max_heapify(arr, n, i)
+    return arr
+
+
+if __name__ == "__main__":
+    data = [64, 34, 25, 12, 22, 11, 90]
+    print("Original array:", data)
+    print("Max Heap:", build_max_heap(data.copy()))
